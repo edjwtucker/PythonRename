e69d5f8659c3d5059db54b8f33014f955ceabae9
edjwtucker <edwardjwtucker@gmail.com>
2017-07-16 17:45:10 +0100

    Initial commit of PythonRename.py and test suite TestPythonRename.py.

---
+++ PythonRename.py
@@ -0,0 +1,90 @@
+# Copyright (c) 2017, Ed Tucker
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+#
+# PythonRename is a unit that renames all files in a directory (non-recursive) 
+# according to the file creation date-time, with a possible appended tag.
+# This module was written to deal with the intensely annoying Android habit of 
+# naming photos DSC_XXXX_Y.png, thus upsetting the ordering of your photos when
+# moving between device and SD Card storage.  
+
+import sys
+from os import path, listdir, rename 
+from time import strftime, gmtime
+
+
+def count_files_and_folders(directory):
+    number_of_files = len(listdir(directory))
+    print ("Number of Files and Folders in {} = {}".format(directory, number_of_files))
+    return number_of_files
+
+
+def listdir_fullpath(directory):
+    return [path.join(directory, f) for f in listdir(directory)]
+
+
+def create_filename(time, tag, ext, number = None):
+    if number == None:
+        new_filename = time + "_" + tag + "." + ext
+    else:
+        new_filename = time + "_" + str(number) + "_" + tag + "." + ext
+    return new_filename
+
+    
+def create_unique_filename(directory, time, tag, ext):
+    ''' Create a unique filename from a time string, file tag, and extension
+        If the created filename already exists, this function will create a 
+        new filename with FILENAME_X where X is an integer. '''
+    potential_filename = create_filename(time, tag, ext, None)
+
+    number = 1
+    while path.isfile(path.join(directory, potential_filename)):
+        potential_filename = create_filename(time, tag, ext, number)
+        number += 1
+
+    return potential_filename
+
+    
+def rename_files_by_datetime(directory, tag = None):
+    ''' Method that recursively renames all files in a directory, using a 
+        filename format based upon their modified time.
+        INPUTS:
+        directory: either the full path or a directory in the same folder as the execution path.
+        tag: Used to add a tag to the renamed files. '''
+    
+    starting_number_of_files = count_files_and_folders(directory)
+
+    for filename in listdir_fullpath(directory):
+        if path.isfile(filename):
+            filepath = path.join(directory, filename)
+
+            # Get file creation time
+            t = strftime('%Y%m%d_%H%M%S', gmtime(path.getmtime(filepath)))
+            ext = filename.split(".")[-1]
+
+            new_filename = create_unique_filename(directory, t, tag, ext)
+            new_file_path = path.join(directory, new_filename)
+            print ('Rename ', filepath, ' as ', new_file_path)
+
+            rename(filepath, new_file_path)
+
+    end_number_of_files = count_files_and_folders(directory)
+    print("Lost files = ", starting_number_of_files - end_number_of_files)
+    print("Completed renaming files")
+    
+    
+if __name__ == '__main__':
+    # Usage:  PythonRename.py  DIRECTORY  TAG
+    # Command line arguments
+    if sys.argv[1] !=  None:
+        short_directory = sys.argv[1]
+        directory = path.abspath(short_directory)
+        print ("Renaming files in directory:", directory)
+
+    tag = None
+    if sys.argv[2] !=  None:
+        tag = sys.argv[2]
+
+    rename_files_by_datetime(directory, tag)
---
+++ test/TestPythonRename.py
@@ -0,0 +1,168 @@
+# Copyright (c) 2017, Ed Tucker
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+#
+# Test suite for the PythonRename unit.
+
+import unittest
+import os
+import shutil
+
+import sys
+sys.path.append('../')
+import PythonRename as rename
+
+# Global variables
+path = os.getcwd()
+
+test_directory = "TestFiles"
+
+test_file_details =  [{'name': 'File-1.png', 'content': '1234'}, 
+                    {'name': 'File_!$%^&.doc', 'content': '98765'},
+                    {'name': '___ext.JPEG', 'content': 'asdfg'},
+                    {'name': 'DSC_1000.PNG', 'content': 'qwerty brain'},
+                    {'name': 'File_with_same_timestamp.gif', 'content': 'what is going on?'}]
+
+test_folder_details = [{'name': 'Folder1', 'files': ['afile.png']},
+                       {'name': 'Folder_2', 'files': ['ajpegfile.jpg', 'anotherfile.txt']}]
+
+test_default_content = 'None'
+
+test_tag = "1!%_abc"
+
+
+# Utilities
+def get_number_of_files_in_directory(directory):
+    number_of_files = sum(os.path.isfile(os.path.join(directory, f)) for f in os.listdir(directory))
+    print('Number of files = ', number_of_files)
+    return number_of_files
+    
+
+def create_file(file_path, content):
+    with open(file_path, 'w') as f:
+        f.write(content)
+
+
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+
+def remove_directory(directory):
+    if directory is not None and os.path.exists(directory):
+        shutil.rmtree(directory)
+
+
+    
+# Test Class for PythonRename module
+class TestPythonRename(unittest.TestCase):
+
+    # test fixture
+    def setUp(self):
+        print("SETUP")
+        
+        # Clean and then make directory for testing
+        self.test_directory = os.path.join(path, test_directory)
+        remove_directory(self.test_directory)
+        create_directory(self.test_directory)
+        self.number_of_files = 0
+        self.number_of_folders = 0
+
+        # Make files to move
+        self.files = test_file_details
+        self.file_contents = []
+        for file in self.files:
+            create_file(self.test_directory + "/" + file['name'], file['content'])
+            self.file_contents.append(file['content'])
+            self.number_of_files += 1
+        
+        # Create subfolders to test non-recursive renaming
+        self.folders = test_folder_details
+        for folder in self.folders:
+            folder['full_path'] = self.test_directory + "/" + folder['name']
+            create_directory(folder['full_path'])
+            self.number_of_folders += 1
+            for file in folder['files']:
+                create_file(folder['full_path'] + "/" + file, test_default_content)
+        
+        self.tag = test_tag
+
+    
+    # count_files_and_folders() test case
+    def test_count_files_and_folders(self):
+        print("test count_files_and_folders()")
+        self.assertEqual(rename.count_files_and_folders(self.test_directory), 
+            self.number_of_files+self.number_of_folders)
+
+
+    # create_filename() test case
+    def test_create_filename(self):
+        print("test create_filename()")
+        # Test without number input
+        expected_filename = '20170716_160100_TEST.jpg'
+        self.assertEqual(rename.create_filename('20170716_160100', 'TEST', 'jpg', None), expected_filename)
+
+        # Test with number input
+        expected_filename = '20170716_160100_10_TEST.jpg'
+        self.assertEqual(rename.create_filename('20170716_160100', 'TEST', 'jpg', 10), expected_filename)
+
+
+    # create_unique_filename() test case    
+    def test_create_unique_filename(self):
+        print("test create_unique_filename()")
+        # Test creation of file that does not already exist
+        expected_filename = '20170716_160100_TEST.jpg'
+        self.assertEqual(rename.create_unique_filename(self.test_directory, '20170716_160100', 'TEST', 'jpg'), expected_filename)
+
+        # Test creation of file that does already exist
+        expected_filename = '20170716_160100_1_TEST.jpg'
+        new_directory = 'NewTestFolder'
+        create_directory(new_directory)
+        create_file(new_directory + "/" + '20170716_160100_TEST.jpg', 'content')            
+        self.assertEqual(rename.create_unique_filename(new_directory, '20170716_160100', 'TEST','jpg'), expected_filename)
+        remove_directory(new_directory)
+
+
+    # rename_files_by_datetime() test case
+    def test_rename_files_by_datetime(self):
+        # Rename files created during setup and confirm output
+        print("test rename_files_by_datetime()")
+        
+        # Rename files using PythonRename module
+        rename.rename_files_by_datetime(self.test_directory, self.tag)
+
+        # Confirm the correct number of files exists, i.e. we haven't lost any
+        self.assertEqual(get_number_of_files_in_directory(self.test_directory), self.number_of_files)
+        
+        # Confirm sub directories have not been touched
+        for folder in self.folders:
+            self.assertTrue(os.path.exists(folder['full_path']))
+            for file in folder['files']:
+                expected_file = folder['full_path'] + "/" + file
+                self.assertTrue(os.path.exists(expected_file))
+                # Confirm contents are intact
+                with open(expected_file) as f: self.assertEqual(f.read(), test_default_content)
+
+        # Confirm file contents are complete
+        for file in [os.path.join(self.test_directory, f) for f in os.listdir()]:
+            if os.path.isfile(file):
+                # Confirm contents are intact
+                with open(file) as f: self.assertTrue(f.read() in self.file_contents)
+                # Confirm tags have been used correctly
+                if self.tag != None:
+                    self.assertTrue(self.tag in file)
+
+        print("test rename_files_by_datetime() complete")
+        
+
+    # test fixture
+    def tearDown(self):
+        # Remove generated folder and files
+        print("TEARDOWN")
+        remove_directory(self.test_directory)
+
+            
+if __name__ == '__main__':
+    unittest.main()
